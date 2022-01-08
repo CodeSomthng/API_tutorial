47b2fafb700b0b5c2cea857258781579a16a5608
Vladyslav <vladyslav.teleleiko@evowill.com>
2022-01-08 21:01:56 +0200

    rspec add factory_bot, requests tests

---
+++ app/controllers/api/v1/books_controller.rb
@@ -0,0 +1,35 @@
+module Api
+  module V1
+    class BooksController < ApplicationController
+
+      def index
+        render json: Book.all
+      end
+      
+      def create
+        # Initialize a record but not save to database yet
+        book = Book.new(book_params)
+
+        if book.save
+          # status code 201 - record is successfully created
+          render json: book, status: :created
+        else
+          # status code 422 - unprocessable_entity, not create a book
+          render json: book.errors, status: :unprocessable_entity
+        end
+      end
+
+      def destroy
+        Book.find(params[:id]).destroy!
+
+        head :no_content
+      end
+
+      private
+
+      def book_params
+        params.require(:book).permit(:title, :author)
+      end
+    end
+  end
+end
