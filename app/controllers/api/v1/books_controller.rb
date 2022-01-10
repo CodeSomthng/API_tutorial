ef6fa69a13ec4c5ff2bfe04dacbb5cbd4aae6231
Vladyslav <vladyslav.teleleiko@evowill.com>
2022-01-10 14:32:42 +0200

    rspec requests tests add representers of json

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -3,13 +3,14 @@ module Api
     class BooksController < ApplicationController
 
       def index
-        render json: Book.all
+        books = Book.all
+
+        render json: BooksRepresenter.new(books).as_json
       end
       
       def create
         # Initialize a record but not save to database yet
         book = Book.new(book_params)
-
         if book.save
           # status code 201 - record is successfully created
           render json: book, status: :created
--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -1,20 +1,45 @@
 require 'rails_helper'
 
 describe 'Books API', type: :request do
-  let!(:book) { create(:book) }
+  # let!(:book) { create(:book) }
   # let!(:book) { create(:book) }
   
 
   describe 'GET /api/v1/books' do
-    it 'returns all books' do
+    before do
+      FactoryBot.create(:book, title: '1984', author: 'George Orwell')
+    end
 
-      # FactoryBot.create(:book, title: '1984', author: 'George Orwell')
-      # FactoryBot.create(:book, title: 'The Time Machine', author: 'H.G. Wells')
-      get '/api/v1/books', as: :json
-      byebug
+    it 'returns all books' do
+      get api_v1_books_path, as: :json
 
       expect(response).to have_http_status(:success)
       expect(JSON.parse(response.body).size).to eq(1)
     end
   end
+
+  describe 'POST /books' do
+    it 'create a new book' do
+      expect {
+        post '/api/v1/books', params: { book: {title: 'The Martian', author: 'Andy Weir'} }
+    }.to change { Book.count }.from(0).to(1)
+      
+
+      expect(response).to have_http_status(:created)      
+    end
+  end
+
+  describe 'DELETE /books/:id' do
+    let!(:book) { FactoryBot.create(:book, title: 'The Time Machine', author: 'H.G. Wells') }
+
+    it 'deletes a book' do
+      expect {
+        delete "/api/v1/books/#{book.id}"
+    }.to change { Book.count }.from(1).to(0)
+      
+
+      expect(response).to have_http_status(:no_content)
+    end
+  end
+
 end
