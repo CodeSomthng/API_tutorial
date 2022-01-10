2b81eae404e13993ecb4c3f1b7387b3f33560dee
Vladyslav <vladyslav.teleleiko@evowill.com>
2022-01-10 11:39:14 +0200

    Add faker gem

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -1,13 +1,20 @@
 require 'rails_helper'
 
 describe 'Books API', type: :request do
-  it 'returns all books' do
-    FactoryBot.create(:book, title: '1984', author: 'George Orwell')
-    FactoryBot.create(:book, title: 'The Time Machine', author: 'H.G. Wells')
+  let!(:book) { create(:book) }
+  # let!(:book) { create(:book) }
+  
 
-    get '/api/v1/books'
+  describe 'GET /api/v1/books' do
+    it 'returns all books' do
 
-    expect(response).to have_http_status(:success)
-    expect(JSON.parse(response.body).size).to eq(2)
+      # FactoryBot.create(:book, title: '1984', author: 'George Orwell')
+      # FactoryBot.create(:book, title: 'The Time Machine', author: 'H.G. Wells')
+      get '/api/v1/books', as: :json
+      byebug
+
+      expect(response).to have_http_status(:success)
+      expect(JSON.parse(response.body).size).to eq(1)
+    end
   end
 end
